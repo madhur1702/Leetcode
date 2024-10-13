3d43b6e2425064858c5c921c2c5c547e3538e0c0
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2024-10-13 16:02:03 +0530

    Create Remove_linkedlist_elements.cpp

---
+++ Remove_linkedlist_elements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if (head == nullptr) { 
+            return head;
+        }
+
+        while (head != nullptr && head->val == val) {
+            head = head->next;
+        }
+
+        ListNode* p = head;
+        ListNode* q = head;
+
+        while (q != nullptr) {
+            if (q->val == val) {
+                p->next = q->next; 
+            } else {
+                p = q;
+            }
+            q = q->next; 
+        }
+
+        return head;
+    }
+};
