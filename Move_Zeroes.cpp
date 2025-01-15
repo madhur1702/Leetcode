db0adeea7a77f141fd840acd7eb3ba57adc2efd7
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2025-01-15 16:46:20 +0530

    Update Move_Zeroes.cpp

--- Move_Zeroes.cpp
+++ Move_Zeroes.cpp
@@ -3,7 +3,6 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         int j = 0;
-
         for (int i = 0; i < n; i++) {
             if (nums[i] != 0) {
                 nums[j] = nums[i];
