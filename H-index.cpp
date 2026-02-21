473f97ce53c1a96c25ea6c384a5c9ccc2359c2fa
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2026-02-21 20:54:47 +0530

    Fix formatting issues in H-index.cpp

--- H-index.cpp
+++ H-index.cpp
@@ -5,7 +5,7 @@ public:
         int h = 0;
         for(int i=0;i<citations.size();i++)
         {
-            if(citations[i]>=i+1)
+            if(citations[i]>=i+1) 
             {
                 h = i + 1;
             }else
