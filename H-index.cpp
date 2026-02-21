455b2d8636b1a2da10757b9c67019fc0538bb2fd
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2026-02-21 20:54:03 +0530

    Create H-index.cpp

---
+++ H-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end(),greater<int>());
+        int h = 0;
+        for(int i=0;i<citations.size();i++)
+        {
+            if(citations[i]>=i+1)
+            {
+                h = i + 1;
+            }else
+            {
+                break;
+            }
+        }
+        return h;
+    }
+};
