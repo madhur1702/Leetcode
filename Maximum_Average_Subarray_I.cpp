95b531faec6159b6ba1f87e2089d74a4be5b86df
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2026-06-12 22:40:22 +0530

    Update Maximum_Average_Subarray_I.cpp

--- Maximum_Average_Subarray_I.cpp
+++ Maximum_Average_Subarray_I.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
         int n = nums.size();
-        int sum = 0; 
+        int sum = 0;
         for(int i = 0;i<k;i++)
         {
             sum += nums[i];
