6d606885f74d5f7b5050ee4272e272740f552087
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2026-07-22 17:24:22 +0530

    Create  Trapping_Rain_Water.cpp

---
+++ Trapping_Rain_Water.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int lmax = 0, rmax = 0, total = 0;
+        int n = height.size();
+        int l = 0,r = n-1;
+        while (l < r) {
+            if (height[l] <= height[r]) {
+                if (lmax > height[l]) {
+                    total += lmax - height[l];
+                } else {
+                    lmax = height[l];
+                }
+                l++;
+            } else {
+                if (rmax > height[r]) {
+                    total += rmax - height[r];
+                } else {
+                    rmax = height[r];
+                }
+                r--;
+            }
+        }
+        return total;
+    }
+};
