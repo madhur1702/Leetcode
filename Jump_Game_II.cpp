3928ea39aa2f71b81fea0f1d5a55d2433724c831
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2026-08-27 07:06:42 +0530

    Create Jump_Game_II.cpp

---
+++ Jump_Game_II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps = 0,l = 0,r = 0;
+        int n = nums.size();
+        while(r<n-1)
+        {
+            int farthest = 0;
+            for(int i = l;i<=r;i++)
+            {
+                farthest = max(farthest,i + nums[i]);
+            }
+            l = r + 1;
+            r = farthest;
+            jumps++;
+        }
+        return jumps;
+    }
+};
